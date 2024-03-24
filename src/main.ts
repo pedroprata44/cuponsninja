482e1c8068a3acade46753319bc819c1475f5a85
josenunesti <30709981+josenunesti@users.noreply.github.com>
2024-03-24 16:19:59 -0300

    feat: return 0 to negative params

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
 export function sum (a: number, b: number){
+    if (a < 0 || b < 0) {
+        return 0;
+    }
     return a + b; 
 }
--- test/main.test.ts
+++ test/main.test.ts
@@ -4,3 +4,8 @@ test("Deve somar corretamente", function(){
     const result = sum(2,2);
     expect(result).toBe(4); 
 });
+
+test("Should return zero when params is negative", function(){
+    const result = sum(-2,-2);
+    expect(result).toBe(0); 
+});
