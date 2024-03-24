5713757ecf8dc84cbb797f2f49dd705e339e0665
pedroprata44 <contato.pedroprata@gmail.com>
2024-03-24 17:41:07 -0300

    test: add test remote

--- test/main.test.ts
+++ test/main.test.ts
@@ -3,5 +3,5 @@ import consumer from "../src/main";
 test("Should return email", function(){
     const register = new consumer("pedro prata", "12345678901", "pedro@prata", "12345678")
 
-    expect(register.getEmail()).toBe("pedro@prata") 
+    expect(register.getEmail()).toBe("pedro@prata")  
 })
