9f500e49e5c93212615443e4dbdbcdda4a84d715
pedroprata44 <contato.pedroprata@gmail.com>
2024-04-02 10:33:36 -0300

    feat: refactor user validations and add new cpf validations

--- src/User.ts
+++ src/User.ts
@@ -3,39 +3,48 @@ export default class User{
     cpf: string
     email: string
     phone: string
-
     constructor(name:string, cpf:string, email:string, phone:string){
         this.name = name
         this.cpf = cpf
         this.email = email
         this.phone = phone
-        
         this.isValid()
     }
-
     isValid(){
-        const validName:boolean = !(!this.name)
-        const validCpf:boolean = !(!this.cpf || this.cpf.length !== 11)
-        const validEmail:boolean = !(!this.email || !this.email.includes("@"))
-        const validPhone:boolean = !(!this.phone || this.phone.length !== 8)
-
-        if(!validName){throw new Error("Invalid name")}
-        if(!validCpf){throw new Error("Invalid cpf")}
-        if(!validEmail){throw new Error("Invalid email")}
-        if(!validPhone){throw new Error("Invalid phone")}
+        if(!this.isValidName(this.name)) throw new Error("Invalid name")
+        if(!this.isValidEmail(this.email)) throw new Error("Invalid email")
+        if(!this.isValidPhone(this.phone)) throw new Error("Invalid phone")
+        if(!this.isValidCpf(this.cpf)) throw new Error("Invalid cpf")
     }
-
-    validCpf(cpf:string){
-        let isValid:boolean = true
-
-        if(!cpf)isValid = false
-        if(!(cpf.length === 11))isValid = false
-        if(!this.sameDigits) isValid = false
-
-        return isValid
+    isValidName(name:string){
+        return !(!name)
     }
-
-    sameDigits(cpf:string){
-        return !(cpf.split("").every(c => c === cpf[0]))
+    isValidCpf(cpf:string){
+        if(!cpf) return false
+        cpf.replace(/\D/g, "")
+        if(cpf.length !== 11) return false
+        if(this.isSameDigits(this.cpf)) return false
+        const digit1 = this.calculateDigit(this.cpf, 10)
+        const digit2 = this.calculateDigit(this.cpf, 11)
+        let nDigVerific = cpf.substring(cpf.length - 2, cpf.length)
+        const nDigResult = "" + digit1 + "" + digit2
+        return nDigVerific == nDigResult
+    }
+    isSameDigits(cpf:string){
+        return cpf.split("").every(c => c === cpf[0])
+    }
+    calculateDigit(cpf:string, factor: number){
+        let total = 0
+        for(const digit of cpf){
+            if(factor > 1) total += parseInt(digit) * factor--
+        }
+        const rest = total%11
+        return (rest < 2) ? 0 : 11 - rest
+    }
+    isValidEmail(email:string){
+        return !(!this.email || !this.email.includes("@"))
+    }
+    isValidPhone(phone:string){
+        return !(!this.phone || this.phone.length !== 8)
     }
 }
