3d3938e56248272b033c8b685967b528b84aaf30
pedroprata44 <contato.pedroprata@gmail.com>
2024-04-17 14:56:24 -0300

    feat: add new validations to signup

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,23 @@ export function signUp(input: any){
         if(input.email === user.email) throw new Error("This email already exists")
     }
     if(isInvalidName(input.name)) throw new Error("Invalid name")
+    if(isInvalidEmail(input.email)) throw new Error("Invalid email")
+    if(isInvalidCpf(input.cpf)) throw new Error("Invalid cpf")
 }
 
 function isInvalidName(name:string){return !name.match(/[a-zA-Z] [a-zA-Z]+/);}
+
+function isInvalidEmail(email:string){return !email.match(/^(.+)@(.+)$/);}
+
+function isInvalidCpf(cpf:string){
+    if(!cpf) return false
+    cpf = clean(cpf)
+    if(isInvalidLength(cpf)) return false
+    if(allDigitsAreTheSame(cpf)) return false
+}
+
+function allDigitsAreTheSame(cpf:string){return cpf.split("").every(c => c === cpf[0])}
+
+function isInvalidLength(cpf: string){return cpf.length !== 11}
+
+function clean(cpf:string){return cpf.replace(/\D/g, "")}
