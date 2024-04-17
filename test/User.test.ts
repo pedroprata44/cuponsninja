31a7214f42a605b144eebc16385742240f5f241e
Pedro Prata <contato.pedroprata@gmail.com>
2024-04-16 23:53:25 -0300

    refactor: user test

--- test/User.test.ts
+++ test/User.test.ts
@@ -1,7 +1,7 @@
 import User from "../src/User";
 
 test.each([""])("Should not create user with invalid name", function(name:any){
-    expect(()=> new User("","12345678901","user@user","12345678")).toThrow("Invalid name")
+    expect(()=> new User(name,"12345678901","user@user","12345678")).toThrow("Invalid name")
 })
 test.each(["", "123456789012", "123456789", "11111111111"])("Should not create user with invalid cpf", function(cpf: any){
     expect(()=> new User("user", cpf, "user@user", "12345678")).toThrow("Invalid cpf")
