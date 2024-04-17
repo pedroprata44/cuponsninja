352b1705157d39e68c71832915fabd484dd1342b
pedroprata44 <contato.pedroprata@gmail.com>
2024-04-17 15:03:30 -0300

    test: add new tests to signup validations

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,10 +1,14 @@
 import { signUp } from "../src/main";
 
 test("Should not do signup with a email already exists", function(){
-    const inputSignup = {name:"user", cpf:"12345678901", email:"user@user", phone:"123456789"}
+    const inputSignup = {name:"user user", cpf:"12345678901", email:"user@user", phone:"123456789"}
     expect(() => signUp(inputSignup)).toThrow(new Error("This email already exists"))
 })
-test.each([""])("Should not do signup with a invalid name", function(name:string){
+test.each(["", "user"])("Should not do signup with a invalid name", function(name:string){
     const inputSignup = {name:name, cpf:"12345678901", email:"@", phone:"123456789"}
     expect(() => signUp(inputSignup)).toThrow(new Error("Invalid name"))
+})
+test.each(["","mate.mate"])("Should not do signup with a invalid email", function(email:string){
+    const inputSignup = {name:"mate mate", cpf:"12345678901", email:email, phone:"123456789"}
+    expect(() => signUp(inputSignup)).toThrow(new Error("Invalid email"))
 })
