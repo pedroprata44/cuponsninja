4764663e647cefd6a48d8f92f3273b465e92a33b
Pedro Prata <contato.pedroprata@gmail.com>
2024-04-16 23:54:44 -0300

    feat: add new use case signup and firts validations

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
-import User from "./User";
+import crypto from "crypto"
 
-let registerUsers: User[] = []
-export {registerUsers}
-
-export default function signUp(user:User){
-    registerUsers.push(user)
-    return registerUsers.slice(-1)
+interface user{
+    name:string
+    cpf:string
+    email:string
+    phone:string
 }
+
+const users: user[] = []
+users.push({name:"user", cpf:"12345678901", email:"user@user", phone:"123456789"})
+
+export function signUp(input: any){
+    const userId = crypto.randomUUID()
+    for(const user of users){
+        if(input.email === user.email) throw new Error("This email already exists")
+    }
+    if(isInvalidName(input.name)) throw new Error("Invalid name")
+}
+
+function isInvalidName(name:string){return !name.match(/[a-zA-Z] [a-zA-Z]+/);}
