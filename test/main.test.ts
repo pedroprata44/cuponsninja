73499fdb322784c0c1cb29683c71dae6cf47bd4e
pedroprata44 <contato.pedroprata@gmail.com>
2024-04-18 14:57:07 -0300

    test: add new validations to cpf and firsts validations to cnpj for companys

--- test/main.test.ts
+++ test/main.test.ts
@@ -12,3 +12,11 @@ test.each(["","mate.mate"])("Should not do signup with a invalid email", functio
     const inputSignup = {name:"mate mate", cpf:"12345678901", email:email, phone:"123456789"}
     expect(() => signUp(inputSignup)).toThrow(new Error("Invalid email"))
 })
+test.each(["", undefined, null, "12345678", "11111111111"])("Should not do signup with a invalid cpf", function(cpf:any){
+    const inputSignup = {name: "mate mate", cpf:cpf, email: "mate@mate", phone:"123456789"}
+    expect(() => signUp(inputSignup)).toThrow(new Error("Invalid cpf"))
+})
+test.each(["", undefined, null, "1111111111111"])("Should not do signup with a invalid cnpj", function(cnpj:any){
+    const inputSignup = {name:"user user", cpf:"17274496069", email:"mate@mate", phone:"123456789", isCompany: true, cnpj:cnpj}
+    expect(() => signUp(inputSignup)).toThrow(new Error("Invalid cnpj"))
+})
