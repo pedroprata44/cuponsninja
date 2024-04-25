01ad008b91123df0b3ece7f8ae5055feaa85b3a9
pedroprata44 <contato.pedroprata@gmail.com>
2024-04-25 14:23:11 -0300

    test: add test to validate the id return of the last signup user

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,12 +1,9 @@
-import { signUp } from "../src/main";
+import { signUp, signUpUser, users } from "../src/main";
+
+//signup user
 
 test("Should not do signUpUser with a user's email already exists", function(){
     const inputSignup = {name:"user user", cpf:"265.092.150-12", email:"user@user", phone:"123456789"}
-    expect(() => signUp(inputSignup)).toThrow(new Error("This email already exists"))
-})
-
-test("Should not do signUpCompany with a company's email already exists", function(){
-    const inputSignup = {name:"user user", cnpj: "65199380000180", email:"company@company", phone:"123456789", isCompany: true}
     expect(() => signUp(inputSignup)).toThrow(new Error("This email already exists"))
 })
 
@@ -28,6 +25,18 @@ test.each(["", undefined, null, "12345678", "11111111111"])("Should not do signU
 test.each(["", undefined, null, "12345678", "1234567890"])("Should not do signUpUser with a invalid phone", function(phone:any){
     const inputSignup = {name:"mate mate", cpf:"265.092.150-12", email:"mate@mate", phone:phone}
     expect(() => signUp(inputSignup)).toThrow(new Error("Invalid phone"))
+})
+
+test("Should return the last user id created", function(){
+    const inputSignup = {name:"mate mate", cpf:"265.092.150-12", email:"test@test", phone: "123456789"}
+    expect(signUpUser(inputSignup)).toBe(users[users.length - 1].id)
+})
+
+//signup company
+
+test("Should not do signUpCompany with a company's email already exists", function(){
+    const inputSignup = {name:"user user", cnpj: "65199380000180", email:"company@company", phone:"123456789", isCompany: true}
+    expect(() => signUp(inputSignup)).toThrow(new Error("This email already exists"))
 })
 
 test.each(["", undefined, null, "1111111111111","11111111111111", "111"])("Should not do signup with a invalid cnpj", function(cnpj:any){
