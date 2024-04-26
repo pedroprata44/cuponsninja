e22bc90c2ad285de3f4b4c36bf86ce6215b08fae
pedroprata44 <contato.pedroprata@gmail.com>
2024-04-26 11:03:38 -0300

    test: refact tests to fix bug that allowed signup user with invalid fields

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,45 +1,27 @@
-import { signUp, signUpUser, users } from "../src/main";
+import { signUp, users } from "../src/main";
 
-//signup user
+//signup user tests
 
-test("Should not do signUpUser with a user's email already exists", function(){
-    const inputSignup = {name:"user user", cpf:"265.092.150-12", email:"user@user", phone:"123456789"}
-    expect(() => signUp(inputSignup)).toThrow(new Error("This email already exists"))
+test("Should not do signup user with a email already exists", function(){
+    expect(() => signUp({email:"user@user"})).toThrow(new Error("This email already exists"))
 })
 
-test.each(["", "user"])("Should not do signUpUser with a invalid name", function(name:string){
-    const inputSignup = {name:name, cpf:"265.092.150-12", email:"mate@mate", phone:"123456789"}
-    expect(() => signUp(inputSignup)).toThrow(new Error("Invalid name"))
+test.each([undefined, null, "", "user"])("Should not do signup user with a invalid name", function(name:any){
+    expect(() => signUp({email:"mate@mate", name:name})).toThrow(new Error("Invalid name"))
 })
 
-test.each(["","mate.mate"])("Should not do signUpUser with a invalid email", function(email:string){
-    const inputSignup = {name:"mate mate", cpf:"265.092.150-12", email:email, phone:"123456789"}
-    expect(() => signUp(inputSignup)).toThrow(new Error("Invalid email"))
+test.each([undefined, null, "", "m.m"])("Should not do signup user with a invalid email", function(email:any){
+    expect(() => signUp({email:email, name:"user user"})).toThrow(new Error("Invalid email"))
 })
 
-test.each(["", undefined, null, "12345678", "11111111111"])("Should not do signUpUser with a invalid cpf", function(cpf:any){
-    const inputSignup = {name: "mate mate", cpf:cpf, email: "mate@mate", phone:"123456789"}
-    expect(() => signUp(inputSignup)).toThrow(new Error("Invalid cpf"))
+test.each([undefined, null, "", "111", "11111111111", "11111111111"])("Should not do signup user with a invalid cpf", function(cpf:any){
+    expect(() => signUp({email:"mate@mate", name: "user user", cpf:cpf})).toThrow(new Error("Invalid cpf"))
 })
 
-test.each(["", undefined, null, "12345678", "1234567890"])("Should not do signUpUser with a invalid phone", function(phone:any){
-    const inputSignup = {name:"mate mate", cpf:"265.092.150-12", email:"mate@mate", phone:phone}
-    expect(() => signUp(inputSignup)).toThrow(new Error("Invalid phone"))
+test.each([undefined, null, "", "12345678"])("Should not do signup user with a invalid phone", function(phone:any){
+    expect(() => signUp({email:"mate@mate", name: "user user", cpf: "91015490069", phone:phone})).toThrow(new Error("Invalid phone"))
 })
 
-test("Should return the last user id created", function(){
-    const inputSignup = {name:"mate mate", cpf:"265.092.150-12", email:"test@test", phone: "123456789"}
-    expect(signUpUser(inputSignup)).toBe(users[users.length - 1].id)
-})
-
-//signup company
-
-test("Should not do signUpCompany with a company's email already exists", function(){
-    const inputSignup = {name:"user user", cnpj: "65199380000180", email:"company@company", phone:"123456789", isCompany: true}
-    expect(() => signUp(inputSignup)).toThrow(new Error("This email already exists"))
-})
-
-test.each(["", undefined, null, "1111111111111","11111111111111", "111"])("Should not do signup with a invalid cnpj", function(cnpj:any){
-    const inputSignup = {name:"user user", cpf:"17274496069", email:"mate@mate", phone:"123456789", isCompany: true, cnpj:cnpj}
-    expect(() => signUp(inputSignup)).toThrow(new Error("Invalid cnpj"))
+test("Should return the correct user id", function(){
+    expect(signUp({email:"mate@mate", name: "user user", cpf: "91015490069", phone: "123456789"})).toBe(users[users.length - 1].id)
 })
