bfbbd2dafe05ec97b1f432674150f83f67779f3f
Pedro Prata <contato.pedroprata@gmail.com>
2024-05-01 21:23:18 -0300

    test: add new tests to new signup flow with the database to users and companys

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,65 +1,49 @@
-import { companys, signUp, users } from "../src/main";
+import { signUp } from "../src/main";
 
 //signup user tests
 
-test("Should not do signup user with a email already exists", function(){
-    expect(() => signUp({email:"user@user"})).toThrow(new Error("This email already exists"))
+test("Should not do signup user with a email already exists", async function(){
+    const email = `user${Math.random()}@user`
+    await signUp({email: email, name: "user user", cpf: "91015490069", phone: "(99) 9999-9999"})
+    await expect(() => signUp({email:email})).rejects.toThrow(new Error("This email already exists"))
 })
 
-test.each([undefined, null, "", "user"])("Should not do signup user with a invalid name", function(name:any){
-    expect(() => signUp({email:"mate@mate", name:name})).toThrow(new Error("Invalid name"))
+test.each([undefined, null, "", "user"])("Should not do signup user with a invalid name", async function(name:any){
+    await expect(() => signUp({email:"mate@mate", name:name})).rejects.toThrow(new Error("Invalid name"))
 })
 
-test.each([undefined, null, "", "m.m"])("Should not do signup user with a invalid email", function(email:any){
-    expect(() => signUp({email:email, name:"user user"})).toThrow(new Error("Invalid email"))
+test.each([undefined, null, "", "m.m"])("Should not do signup user with a invalid email", async function(email:any){
+    await expect(() => signUp({email:email, name:"user user"})).rejects.toThrow(new Error("Invalid email"))
 })
 
-test.each([undefined, null, "", "111", "11111111111", "11111111111"])("Should not do signup user with a invalid cpf", function(cpf:any){
-    expect(() => signUp({email:"mate@mate", name: "user user", cpf:cpf})).toThrow(new Error("Invalid cpf"))
+test.each([undefined, null, "", "111", "11111111111", "11111111111"])("Should not do signup user with a invalid cpf", async function(cpf:any){
+    await expect(() => signUp({email:"mate@mate", name: "user user", cpf:cpf})).rejects.toThrow(new Error("Invalid cpf"))
 })
 
-test.each([undefined, null, "", "() 0000-0000", "(00) 00000000", "0000000000"])("Should not do signup user with a invalid phone", function(phone:any){
-    expect(() => signUp({email:"mate@mate", name: "user user", cpf: "91015490069", phone:phone})).toThrow(new Error("Invalid phone"))
-})
-
-test("Should return the right user id", function(){
-    expect(signUp({email:"mate@mate", name: "user user", cpf: "91015490069", phone: "(99) 9999-9999"})).toBe(users[users.length - 1].id)
-})
-
-test("Should get signup date of user", function(){
-    for(const user of users){
-        expect(user.dateSignup).toBeDefined()
-    }
+test.each([undefined, null, "", "() 0000-0000", "(00) 00000000", "0000000000"])("Should not do signup user with a invalid phone", async function(phone:any){
+    await expect(() => signUp({email:"mate@mate", name: "user user", cpf: "91015490069", phone:phone})).rejects.toThrow(new Error("Invalid phone"))
 })
 
 //signup company tests
 
-test("Should not do sign up company with a email already exists", function(){
-    expect(() => signUp({isCompany:true, email:"company@company"})).toThrow(new Error("This email already exists"))
+test("Should not do sign up company with a email already exists", async function(){
+    const email = `company${Math.random()}@company`
+    await signUp({isCompany:true, email:email, name: "company company", cnpj: "83800838000197", phone: "(99) 9999-9999"})
+    await expect(() => signUp({isCompany:true, email:email})).rejects.toThrow(new Error("This email already exists"))
 })
 
-test.each([undefined, null, "", "company"])("Shoud not do signup company with a invalid name", function(name:any){
-    expect(() => signUp({isCompany:true, email:"mate@mate", name:name})).toThrow(new Error("Invalid name"))
+test.each([undefined, null, "", "company"])("Shoud not do signup company with a invalid name", async function(name:any){
+    await expect(() => signUp({ isCompany: true, email: "mate@mate", name: name })).rejects.toThrow(new Error("Invalid name"))
 })
 
-test.each([undefined, null, "", "mate.mate"])("Should not do signup company with a invalid email", function(email:any){
-    expect(() => signUp({isCompany:true, email:email, name:"company company"})).toThrow(new Error("Invalid email"))
+test.each([undefined, null, "", "mate.mate"])("Should not do signup company with a invalid email", async function(email:any){
+    await expect(() => signUp({isCompany:true, email:email, name:"company company"})).rejects.toThrow(new Error("Invalid email"))
 })
 
-test.each([undefined, null, "", "111", "11111111111111", "83800838000155"])("Should not do signup company with a invalid cnpj", function(cnpj:any){
-    expect(() => signUp({isCompany:true, email:"mate@mate", name:"company company", cnpj:cnpj})).toThrow("Invalid cnpj")
+test.each([undefined, null, "", "111", "11111111111111", "83800838000155"])("Should not do signup company with a invalid cnpj", async function(cnpj:any){
+    await expect(() => signUp({isCompany:true, email:"mate@mate", name:"company company", cnpj:cnpj})).rejects.toThrow("Invalid cnpj")
 })
 
-test.each([undefined, null, "", "() 0000-0000", "(00) 00000000", "0000000000"])("Should not do signup company with a invalid phone", function(phone:any){
-    expect(() => signUp({isCompany:true, email:"mate@mate", name: "company company", cnpj:"83800838000197", phone:phone})).toThrow(new Error("Invalid phone"))
-})
-
-test("Should return the correct company id", function(){
-    expect(signUp({isCompany:true, email:"mate@mate", name: "company company", cnpj:"83800838000197", phone:"(99) 9999-9999"})).toBe(companys[companys.length - 1].id)
-})
-
-test("Should get signup date of company", function(){
-    for(const company of companys){
-        expect(company.dateSignup).toBeDefined()
-    }
+test.each([undefined, null, "", "() 0000-0000", "(00) 00000000", "0000000000"])("Should not do signup company with a invalid phone", async function(phone:any){
+    await expect(() => signUp({isCompany:true, email:"mate@mate", name: "company company", cnpj:"83800838000197", phone:phone})).rejects.toThrow(new Error("Invalid phone"))
 })
