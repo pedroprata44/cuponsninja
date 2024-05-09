52ea2fecdce366437543b6808aa4788f71bee197
pedroprata44 <contato.pedroprata@gmail.com>
2024-05-09 15:36:33 -0300

    test: init adapt tests to api

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,5 +1,7 @@
 import axios from "axios"
-import { signUp } from "../src/main"
+axios.defaults.validateStatus = function (){
+    return true
+}
 
 test("Do user signup by api", async function(){
     const inputSignup = {
@@ -34,41 +36,87 @@ test("Do company signup by api", async function(){
     expect(outputGetAccount.email).toBe(inputSignup.email)
 })
 
+//signup user tests
 
+test("Should not do signup user with a email already exists", async function(){
+    const inputSignup = {
+        name: "user user",
+        cpf: "91015490069",
+        email: `user${Math.random()}@user`,
+        phone: "(99) 9999-9999"
+    }
+    await axios.post("http://localhost:3000/signup", inputSignup)
+    const responseSignup = await axios.post("http://localhost:3000/signup", inputSignup)
+    const outputSignup = responseSignup.data
+    expect(responseSignup.status).toBe(422)
+    expect(outputSignup.message).toBe("This email already exists")
+})
+test.each([undefined, null, "", "user"
 
-// import { signUp } from "../src/main";
+])("Should not do signup user with a invalid name", async function(name:any){
+    const inputSignup = {
+        email: `user${Math.random()}@user`,
+        name: name
+    }
+    const responseSignup = await axios.post("http://localhost:3000/signup", inputSignup)
+    expect(responseSignup.status).toBe(422)
+    const outputSignup = responseSignup.data
+    expect(outputSignup.message).toBe("Invalid name")
+})
 
-// //signup user tests
+test.each([undefined, null, "", "m.m"]
+)("Should not do signup user with a invalid email", async function(email:any){
+    const inputSignup = {
+        email: email,
+        name: "user user"
+    }
+    const responseSignup = await axios.post("http://localhost:3000/signup", inputSignup)
+    expect(responseSignup.status).toBe(422)
+    const outputSignup = responseSignup.data
+    expect(outputSignup.message).toBe("Invalid email")    
+})
 
-// test("Should not do signup user with a email already exists", async function(){
-//     const email = `user${Math.random()}@user`
-//     await signUp({email: email, name: "user user", cpf: "91015490069", phone: "(99) 9999-9999"})
-//     await expect(() => signUp({email:email})).rejects.toThrow(new Error("This email already exists"))
-// })
+test.each([undefined, null, "", "111", "11111111111", "11111111111", "46890347810"]
+)("Should not do signup user with a invalid cpf", async function(cpf:any){
+    const inputSignup = {
+        email: `user${Math.random()}@user`,
+        name: "user user",
+        cpf: cpf
+    }
+    const responseSignup = await axios.post("http://localhost:3000/signup", inputSignup)
+    expect(responseSignup.status).toBe(422)
+    const outputSignup = responseSignup.data
+    expect(outputSignup.message).toBe("Invalid cpf")  
+})
 
-// test.each([undefined, null, "", "user"])("Should not do signup user with a invalid name", async function(name:any){
-//     await expect(() => signUp({email:"mate@mate", name:name})).rejects.toThrow(new Error("Invalid name"))
-// })
+test.each([undefined, null, "", "() 0000-0000", "(00) 00000000", "0000000000"]
+)("Should not do signup user with a invalid phone", async function(phone:any){
+    const inputSignup = {
+        email: `user${Math.random()}@user`,
+        name: "user user",
+        cpf: "91015490069",
+        phone: phone
+    }
+    const responseSignup = await axios.post("http://localhost:3000/signup", inputSignup)
+    expect(responseSignup.status).toBe(422)
+    const outputSignup = responseSignup.data
+    expect(outputSignup.message).toBe("Invalid phone")
+})
 
-// test.each([undefined, null, "", "m.m"])("Should not do signup user with a invalid email", async function(email:any){
-//     await expect(() => signUp({email:email, name:"user user"})).rejects.toThrow(new Error("Invalid email"))
-// })
+//signup company tests
 
-// test.each([undefined, null, "", "111", "11111111111", "11111111111"])("Should not do signup user with a invalid cpf", async function(cpf:any){
-//     await expect(() => signUp({email:"mate@mate", name: "user user", cpf:cpf})).rejects.toThrow(new Error("Invalid cpf"))
-// })
-
-// test.each([undefined, null, "", "() 0000-0000", "(00) 00000000", "0000000000"])("Should not do signup user with a invalid phone", async function(phone:any){
-//     await expect(() => signUp({email:"mate@mate", name: "user user", cpf: "91015490069", phone:phone})).rejects.toThrow(new Error("Invalid phone"))
-// })
-
-// //signup company tests
-
-// test("Should not do sign up company with a email already exists", async function(){
-//     const email = `company${Math.random()}@company`
-//     await signUp({isCompany:true, email:email, name: "company company", cnpj: "83800838000197", phone: "(99) 9999-9999"})
-//     await expect(() => signUp({isCompany:true, email:email})).rejects.toThrow(new Error("This email already exists"))
-// })
+test("Should not do sign up company with a email already exists", async function(){
+    const inputSignup = {
+        isCompany: true,
+        name: "company company",
+        cnpj: "83800838000197",
+        email: `company${Math.random()}@company`,
+        phone: "(99) 9999-9999"
+    }
+    await axios.post("http://localhost:3000/signup", inputSignup)
+    const responseSignup = await axios.post("http://localhost:3000/signup", inputSignup)
+    expect(responseSignup.status).toBe(422)
+})
 
 // test.each([undefined, null, "", "company"])("Shoud not do signup company with a invalid name", async function(name:any){
 //     await expect(() => signUp({ isCompany: true, email: "mate@mate", name: name })).rejects.toThrow(new Error("Invalid name"))
