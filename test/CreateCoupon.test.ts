c8f3ab695317cef090524fb025f680b84fc227b9
pedroprata44 <contato.pedroprata@gmail.com>
2024-05-23 15:27:41 -0300

    test: add tests to validations of coupon create

--- test/CreateCoupon.test.ts
+++ test/CreateCoupon.test.ts
@@ -1,15 +1,21 @@
+import CompanyDAODatabase from "../src/CompanyDAODatabase"
 import CouponDAODatabase from "../src/CouponDAODatabase"
 import CreateCoupon from "../src/CreateCoupon"
+import GetCoupon from "../src/GetCoupon"
 import LoggerConsole from "../src/LoggerConsole"
 
 let createCoupon: CreateCoupon
+let getCoupon: GetCoupon
 let logger: LoggerConsole
 let couponDAO: CouponDAODatabase
+let companyDAO: CompanyDAODatabase
 
 beforeEach(() => {
-    logger = new LoggerConsole
+    logger = new LoggerConsole()
     couponDAO = new CouponDAODatabase()
-    createCoupon = new CreateCoupon(logger, couponDAO)
+    companyDAO = new CompanyDAODatabase
+    createCoupon = new CreateCoupon(logger, couponDAO, companyDAO)
+    getCoupon = new GetCoupon(couponDAO)
 })
 
 test("Should create a coupon", async function(){
@@ -20,8 +26,26 @@ test("Should create a coupon", async function(){
     }
     
     const outputCreate = await createCoupon.execute(inputCreate)
-    const outputGetCoupon = await couponDAO.getById(outputCreate.couponId)
+    const outputGetCoupon = await getCoupon.execute(outputCreate.couponId)
     expect(outputGetCoupon.id).toBeDefined()
     expect(outputGetCoupon.describe).toBe(inputCreate.describe)
     expect(outputGetCoupon.quantity).toBe(inputCreate.quantity)
+})
+
+test.each([undefined, null, "", "1634919c-7f5f-49a3-9c2d-24e0e1b1c6b2"])("Should not create coupon with a invalid createdBy", async function(createdBy: any){
+    const inputCreate = {
+        createdBy: createdBy,
+        describe: "describe",
+        quantity: 1
+    }
+    await expect(createCoupon.execute(inputCreate)).rejects.toThrow(new Error("Invalid created by"))
+})
+
+test.each([undefined, null, "", "ddddddddddddddddddddddddddddddd"])("Should not create coupon with a invalid describe", async function(describe: any){
+    const inputCreate = {
+        createdBy: "1634919c-7f5f-49a3-9c2d-24e0e1b1c6b1",
+        describe: describe,
+        quantity: 1
+    }
+    await expect(createCoupon.execute(inputCreate)).rejects.toThrow(new Error("Invalid describe"))
 })
