09746be318af7d609701cd6c97bf1e16372f81c7
Pedro Prata <contato.pedroprata@gmail.com>
2024-05-24 01:33:43 -0300

    feat: add validation in quantity field in coupon create

--- src/CouponCreate.ts
+++ src/CouponCreate.ts
@@ -15,11 +15,16 @@ export default class CouponCreate{
         this.logger.log(input)
         if(await this.isInvalidCreatedBy(input.createdBy)) throw new Error("Invalid created by")
         if(this.isInvalidDescribe(input.describe)) throw new Error("Invalid describe")
+        if(this.isInvalidQuantity(input.quantity)) throw new Error("Invalid quantity")
         input.id = crypto.randomUUID()
         await this.couponDAO.save(input)
         return{
             couponId: input.id
         }
+    }
+    isInvalidQuantity(quantity: any){
+        if(!quantity) return true
+        return !(quantity >= 0)
     }
     isInvalidDescribe(describe: string){
         if(!describe) return true
