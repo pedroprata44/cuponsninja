df7985bc0772f9969df8c8aa022ce06d0084c378
pedroprata44 <contato.pedroprata@gmail.com>
2024-05-28 13:31:28 -0300

    feat: add validations to coupon id and coupon quantity in the coupon consume

--- src/CouponConsume.ts
+++ src/CouponConsume.ts
@@ -1,9 +1,12 @@
 import CouponConsumeDAO from "./CouponConsumeDAO";
 
 export default class CouponConsume{
-    constructor(private couponDAO: CouponConsumeDAO){    
+    constructor(private couponConsumeDAO: CouponConsumeDAO){    
     }
     async execute(couponId: string){
-        await this.couponDAO.update(couponId)
+        const coupon = await this.couponConsumeDAO.getById(couponId)
+        if(!coupon) throw new Error("This coupon id not exists")
+        if(!coupon.quantity) throw new Error("This coupon doesn't have enough quantity to be consumed")
+        await this.couponConsumeDAO.update(couponId)
     }
 }
--- src/CouponConsumeDAO.ts
+++ src/CouponConsumeDAO.ts
@@ -1,3 +1,4 @@
 export default interface CouponConsumeDAO{
+    getById(couponId: string): Promise<any>
     update(couponId: string): Promise<void>
 }
