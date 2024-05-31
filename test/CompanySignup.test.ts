f29a7824929eec689f526c30026eb79a763cd15d
pedroprata44 <contato.pedroprata@gmail.com>
2024-05-31 14:53:44 -0300

    test: add unities tests to usersignup test and company signup test

--- test/CompanySignup.test.ts
+++ test/CompanySignup.test.ts
@@ -3,6 +3,7 @@ import CompanyGetDAO from "../src/CompanyGetAccount"
 import CompanySignup from "../src/CompanySignup"
 import LoggerConsole from "../src/LoggerConsole"
 import CompanyDAODatabase from "../src/CompanyDAODatabase"
+import CompanyDAO from "../src/CompanyDAO"
 
 let companySignup: CompanySignup
 let companyGetAccount: CompanyGetDAO
@@ -14,26 +15,18 @@ beforeEach(() => {
     companyGetAccount = new CompanyGetDAO(companyDAO)
 })
 
-test("Should do company signup by SPY", async function(){
-    const spyLoggerLog = sinon.spy(LoggerConsole.prototype, "log")
-    const inputSignup = {
-        isCompany: true,
-        name: "company company",
-        cnpj: "83800838000197",
-        email: `company${Math.random()}@company`,
-        phone: "(99) 9999-9999"
-    }
-    const outputSignup = await companySignup.execute(inputSignup)
-    const outputGetAccount = await companyGetAccount.execute(outputSignup.companyId)
-    expect(outputGetAccount.id).toBeDefined()
-    expect(outputGetAccount.name).toBe(inputSignup.name)
-    expect(outputGetAccount.email).toBe(inputSignup.email)
-    expect(spyLoggerLog.calledOnce).toBeTruthy()
-    expect(spyLoggerLog.calledWith("signup company company company")).toBeTruthy()
-    spyLoggerLog.restore()
-})
-
 test("Should not do sign up company with a email already exists", async function(){
+    const companys: any = []
+    const companyDAO: CompanyDAO = {
+        async save (company: any): Promise<void> {
+            companys.push(company)
+        },
+        async getById (companyId: string): Promise<any> {
+        },
+        async getByEmail(companyEmail: string): Promise<any> {
+            return companys.find((company: any) => company.email === companyEmail)
+        }
+    }
     const inputSignup = {
         isCompany: true,
         name: "company company",
@@ -41,29 +34,35 @@ test("Should not do sign up company with a email already exists", async function
         email: `company${Math.random()}@company`,
         phone: "(99) 9999-9999"
     }
+    const companySignup = new CompanySignup(new LoggerConsole(), companyDAO)
     await companySignup.execute(inputSignup)
     await expect(() => companySignup.execute(inputSignup)).rejects.toThrow(new Error("This email already exists"))
 })
 
-test.each([undefined, null, "", "company"])("Shoud not do signup company with a invalid name", async function(name:any){
+test.each([undefined, null, "", "company"])("Shoud not do signup company with a invalid name", function(name:any){
+    const stubCompanyDAOGetByEmail = sinon.stub(CompanyDAODatabase.prototype, "getByEmail").resolves(null)
     const inputSignup = {
         isCompany: true,
         email: `company${Math.random()}@company`,
         name: name
     }
-    await expect(() => companySignup.execute(inputSignup)).rejects.toThrow(new Error("Invalid name"))
+    expect(() => companySignup.execute(inputSignup)).rejects.toThrow(new Error("Invalid name"))
+    stubCompanyDAOGetByEmail.restore()
 })
 
 test.each([undefined, null, "", "company.company"])("Should not do signup company with a invalid email", async function(email:any){
+    const stubCompanyDAOGetByEmail = sinon.stub(CompanyDAODatabase.prototype, "getByEmail").resolves(null)
     const inputSignup = {
         isCompany: true,
         email: email,
         name: "company company"
     }
     await expect(() => companySignup.execute(inputSignup)).rejects.toThrow(new Error("Invalid email"))
+    stubCompanyDAOGetByEmail.restore()
 })
 
 test("Should not do signup company with a invalid cnpj", async function(){
+    const stubCompanyDAOGetByEmail = sinon.stub(CompanyDAODatabase.prototype, "getByEmail").resolves(null)
     const inputSignup = {
         isCompany: true,
         email: `company${Math.random()}@company`,
@@ -71,9 +70,11 @@ test("Should not do signup company with a invalid cnpj", async function(){
         cnpj: "83800838000155"
     }
     await expect(() => companySignup.execute(inputSignup)).rejects.toThrow(new Error("Invalid cnpj"))
+    stubCompanyDAOGetByEmail.restore()
 })
 
 test.each([undefined, null, "", "() 0000-0000", "(00) 00000000", "0000000000"])("Should not do signup company with a invalid phone", async function(phone:any){
+    const stubCompanyDAOGetByEmail = sinon.stub(CompanyDAODatabase.prototype, "getByEmail").resolves(null)
     const inputSignup = {
         isCompany: true,
         email: `company${Math.random()}@company`,
@@ -82,4 +83,5 @@ test.each([undefined, null, "", "() 0000-0000", "(00) 00000000", "0000000000"])(
         phone: phone
     }
     await expect(() => companySignup.execute(inputSignup)).rejects.toThrow(new Error("Invalid phone"))
+    stubCompanyDAOGetByEmail.restore()
 })
--- test/UserSignup.test.ts
+++ test/UserSignup.test.ts
@@ -15,95 +15,7 @@ beforeEach(() => {
     userGetAccount = new UserGetAccount(userDAO)
 })
 
-test("Should do user signup", async function(){
-    const inputSignup = {
-        name: "user user",
-        cpf: "91015490069",
-        email: `user${Math.random()}@user`,
-        phone: "(99) 9999-9999"
-    }
-    const outputSignup = await userSignup.execute(inputSignup)
-    const outputGetAccount = await userGetAccount.execute(outputSignup.userId)
-    expect(outputGetAccount.name).toBe(inputSignup.name)
-    expect(outputGetAccount.email).toBe(inputSignup.email) 
-})
-
-test("Should do user signup by Stub", async function(){
-    const stubUserDAOSave = sinon.stub(UserDAODatabase.prototype, "save").resolves()
-    const stubUserDAOGetByEmail = sinon.stub(UserDAODatabase.prototype, "getByEmail").resolves(null)
-    const inputSignup = {
-        name: "user user",
-        cpf: "91015490069",
-        email: `user${Math.random()}@user`,
-        phone: "(99) 9999-9999"
-    }
-    const outputSignup = await userSignup.execute(inputSignup)
-    expect(outputSignup.userId).toBeDefined()
-    const stubUserDAOGetById = sinon.stub(UserDAODatabase.prototype, "getById").resolves(inputSignup)
-    const outputGetAccount = await userGetAccount.execute(outputSignup.userId)
-    expect(outputGetAccount.name).toBe(inputSignup.name)
-    expect(outputGetAccount.email).toBe(inputSignup.email)
-    stubUserDAOSave.restore()
-    stubUserDAOGetByEmail.restore()
-    stubUserDAOGetById.restore()
-})
-
-test("Should do user signup by Fake", async function(){
-    const inputSignup = {
-        name: "user user",
-        cpf: "91015490069",
-        email: `user${Math.random()}@user`,
-        phone: "(99) 9999-9999"
-    }
-    const userDAO: UserDAO = {
-        async save (user: any): Promise<void> {
-        },
-        async getById (userId: string): Promise<any> {
-            return inputSignup
-        },
-        async getByEmail (userEmail: string): Promise<any> {
-            return undefined
-        }
-    }
-    const logger: Logger = {
-        log (message: string): void {
-        }
-    }
-    const userSignup = new UserSignup(userDAO, logger)
-    const userGetAccount = new UserGetAccount(userDAO)
-    const outputSignup = await userSignup.execute(inputSignup)
-    expect(outputSignup.userId).toBeDefined()
-    const outputGetAccount = await userGetAccount.execute(outputSignup.userId)
-    expect(outputGetAccount.name).toBe(inputSignup.name)
-    expect(outputGetAccount.email).toBe(inputSignup.email)
-
-})
-
-test("Should do user signup by Mock", async function(){
-    const mockLogger = sinon.mock(LoggerConsole.prototype)
-    mockLogger.expects("log").withArgs("signup user user user").calledOnce
-    const inputSignup = {
-        name: "user user",
-        cpf: "91015490069",
-        email: `user${Math.random()}@user`,
-        phone: "(99) 9999-9999"
-    }
-    const outputSignup = await userSignup.execute(inputSignup)
-    expect(outputSignup.userId).toBeDefined()
-    const outputGetAccount = await userGetAccount.execute(outputSignup.userId)
-    expect(outputGetAccount.name).toBe(inputSignup.name)
-    expect(outputGetAccount.email).toBe(inputSignup.email)
-    mockLogger.verify()
-    mockLogger.restore()
-})
-
 test("Should not do signup user with a email already exists", async function(){
-    const inputSignup = {
-        name: "user user",
-        cpf: "91015490069",
-        email: `user${Math.random()}@user`,
-        phone: "(99) 9999-9999"
-    }
     const users: any = []
     const userDAO: UserDAO = {
         async save (user: any): Promise<void> {
@@ -116,8 +28,14 @@ test("Should not do signup user with a email already exists", async function(){
             return users.find((user: any) => user.email === userEmail)
         }
     }
+    const inputSignup = {
+        name: "user user",
+        cpf: "91015490069",
+        email: `user${Math.random()}@user`,
+        phone: "(99) 9999-9999"
+    }
     const userSignup = new UserSignup(userDAO, new LoggerConsole())
-    const outputUserSignup = await userSignup.execute(inputSignup)
+    await userSignup.execute(inputSignup)
     expect(() => userSignup.execute(inputSignup)).rejects.toThrow(new Error("This email already exists"))
 })
 
