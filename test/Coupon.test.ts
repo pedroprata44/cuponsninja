a49ea459ab5a29bd5b1a1bde51421ff93825e412
Pedro Prata <contato.pedroprata@gmail.com>
2024-06-08 01:52:20 -0300

    test: change test to test new invalidquantity in coupon class

--- test/Coupon.test.ts
+++ test/Coupon.test.ts
@@ -1,14 +1,14 @@
 import Coupon from "../src/Coupon"
 
 test("Should create a coupon", function(){
-    const coupon = Coupon.create("", "describe", 1)
+    const coupon = Coupon.create("", "describe", 0)
     expect(coupon.id).toBeDefined()
     expect(coupon.describe).toBe("describe")
-    expect(coupon.quantity).toBe(1)
+    expect(coupon.quantity).toBe(0)
 })
 test.each([undefined, null, "", "cccccccccccccccccccccccccccccc"])("Should not create a coupon with a invalid describe", function(describe: any){
     expect(() => new Coupon(crypto.randomUUID(), "", describe, 1)).toThrow(new Error("Invalid describe"))
 })
-test.each([undefined, null, "", -1])("Should not create a coupon with a invalid quantity", function(quantity: any){
+test.each([null, undefined, "", -1])("Should not create a coupon with a invalid quantity", function(quantity: any){
     expect(() => new Coupon(crypto.randomUUID(), "", "describe", quantity)).toThrow(new Error("Invalid quantity"))
 })
