e02b25c1c56a2eee5820f9aa0f869679f21a5727
Pedro Prata <contato.pedroprata@gmail.com>
2024-06-11 00:04:47 -0300

    feat: add code field to coupon class add discount field to coupon class add validdate fielld to coupon class

--- src/domain/Coupon.ts
+++ src/domain/Coupon.ts
@@ -1,24 +1,44 @@
 export default class Coupon{
     id: string
+    code: string
+    discount: string
+    validDate: Date
     createdBy: string
     describe: string
     quantity: number
     creationDate: string
-    constructor(id: string, createdBy: string, describe: string, quantity: number){
+    constructor(id: string, code: string, discount: string, validDate: Date, createdBy: string, describe: string, quantity: number){
+        if(this.isInvalidCode(code)) throw new Error("Invalid code")
+        if(this.isInvalidDiscount(discount)) throw new Error("Invalid discount")
+        if(this.IsInvalidValidDate(validDate)) throw new Error("Invalid valid date")
         if(this.isInvalidDescribe(describe)) throw new Error("Invalid describe")
         if(this.isInvalidQuantity(quantity)) throw new Error("Invalid quantity")
         this.id = id
+        this.code = code.toUpperCase()
+        this.discount = discount
+        this.validDate = validDate
         this.createdBy = createdBy
         this.describe = describe
         this.quantity = quantity
         this.creationDate = Date.call("")
     }
-    static create(createdBy: string, describe: string, quantity: number){
+    static create(code: string, discount: string, validDate: Date, createdBy: string, describe: string, quantity: number){
         const id = crypto.randomUUID()
-        return new Coupon(id, createdBy, describe, quantity)
+        return new Coupon(id, code, discount, validDate, createdBy, describe, quantity)
     }
-    static restore(id: string, createdBy: string, describe: string, quantity: number){
-        return new Coupon(id, createdBy, describe, quantity)
+    static restore(id: string, code:string, discount: string, validDate: Date, createdBy: string, describe: string, quantity: number){
+        return new Coupon(id, code, discount, validDate, createdBy, describe, quantity)
+    }
+    isInvalidCode(code: string){
+        if(!code || code.length !== 8) return true
+    }
+    isInvalidDiscount(discount: string){
+        if(!discount) return true
+        return !discount.match(/^[0-9]{1,2}\%$/)
+    }
+    IsInvalidValidDate(validDate: Date){
+        if(!validDate) return true
+        if(validDate < new Date()) return true
     }
     isInvalidQuantity(quantity: number){
         if(typeof quantity != "number") return true
