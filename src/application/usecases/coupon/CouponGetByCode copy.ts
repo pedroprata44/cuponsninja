55ea1bf994e9b9b1d84de640e23b5400778f169c
pedroprata44 <contato.pedroprata@gmail.com>
2024-06-11 18:53:29 -0300

    feat: add the coupongetbycode and implements new error messages in getbycode and getbyid

--- src/application/usecases/coupon/CouponGetByCode copy.ts
+++ src/application/usecases/coupon/CouponGetByCode copy.ts
@@ -1,13 +1,13 @@
 import CouponRepository from "../../repository/CouponRepository"
 
-export default class CouponGetById{
+export default class CouponGetByCode{
     couponRepository: CouponRepository
     constructor(couponRepository:CouponRepository){
         this.couponRepository = couponRepository
     }
-    async execute(couponId: string){
-        const coupon = await this.couponRepository.getById(couponId)
-        if(!coupon) throw new Error("This coupon id doesn't exists") 
+    async execute(couponCode: string){
+        const coupon = await this.couponRepository.getByCode(couponCode)
+        if(!coupon) throw new Error("This coupon code doesn't exists") 
         return coupon
     }
 }
