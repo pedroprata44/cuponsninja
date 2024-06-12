dc61d9af4a01c4b54cf1ee471e7e228596dfd6c0
Pedro Prata <contato.pedroprata@gmail.com>
2024-06-12 01:54:01 -0300

    feat: add not create a coupon if coupon code already exists

--- src/application/usecases/coupon/CouponCreate.ts
+++ src/application/usecases/coupon/CouponCreate.ts
@@ -11,6 +11,8 @@ export default class CouponCreate{
         if(this.isInvalidCreatedBy(input.createdBy)) throw new Error("Invalid createdBy") 
         const existingCompany = await this.companyRepository.getById(input.createdBy)
         if(!existingCompany) throw new Error("This company not exists")
+        const existingCoupon = await this.couponRepository.getByCode(input.code)
+        if(existingCoupon) throw new Error("This code coupon already exists")
         const coupon = Coupon.create(input.code, input.discount, input.expirationDate, input.createdBy, input.describe, input.quantity)
         await this.couponRepository.save(coupon)
         return{
