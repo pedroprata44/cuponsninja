b473f1f9ec3f93f178b23ecfbdb8e24048c9f8f5
Pedro Prata <contato.pedroprata@gmail.com>
2024-06-18 00:19:21 -0300

    test: add user id in coupon consume test

--- test/CouponConsume.test.ts
+++ test/CouponConsume.test.ts
@@ -7,8 +7,12 @@ import CouponConsume from "../src/application/usecases/coupon/CouponConsume"
 import CouponCreate from "../src/application/usecases/coupon/CouponCreate"
 import CouponGetById from "../src/application/usecases/coupon/CouponGetById"
 import LoggerConsole from "../src/infra/logger/LoggerConsole"
-import sinon from "sinon"
+import UserRepository from "../src/application/repository/UserRepository"
+import UserRepositoryDatabase from "../src/infra/repository/UserRepositoryDatabase"
+import UserSignup from "../src/application/usecases/user/UserSignup"
 
+let userRepository: UserRepository
+let userSignup: UserSignup
 let couponCreate: CouponCreate
 let couponGetById: CouponGetById
 let couponConsume: CouponConsume
@@ -21,15 +25,24 @@ let databaseConnection: DatabaseConnection
 beforeEach(() => {
     logger = new LoggerConsole()
     databaseConnection = new PgPromiseAdapter()
+    userRepository = new UserRepositoryDatabase(databaseConnection)
+    userSignup = new UserSignup(userRepository, logger)
     couponRepository = new CouponRepositoryDatabase(databaseConnection)
     companyRepository = new CompanyRepositoryDatabase(databaseConnection)
     couponCreate = new CouponCreate(logger, couponRepository, companyRepository)
     couponGetById = new CouponGetById(couponRepository)
-    couponConsume = new CouponConsume(couponRepository)
+    couponConsume = new CouponConsume(couponRepository, userRepository)
     companySignup = new CompanySignup(logger, companyRepository)
 })
 
 test("Should consume a coupon", async function(){
+    const inputUserSignup = {
+        name: "user user",
+        cpf: "91015490069",
+        email: `user${Math.random()}@user`,
+        phone: "(99) 9999-9999"
+    }
+    const outputUserSignup = await userSignup.execute(inputUserSignup)
     const inputCompany = {
         isCompany: true,
         name: "company company",
@@ -48,17 +61,32 @@ test("Should consume a coupon", async function(){
     }
     const outputCouponCreate = await couponCreate.execute(inputCoupon)
     const outputCouponGet = await couponGetById.execute(outputCouponCreate.couponId)
-    await couponConsume.execute(outputCouponGet.id)
-    const couponConsumed = await couponGetById.execute(outputCouponCreate.couponId)
-    expect(couponConsumed.quantity).toBe(outputCouponGet.quantity - 1)
-
+    const outputCouponConsume = await couponConsume.execute({couponId: outputCouponGet.id, userId: outputUserSignup.userId})
+    const outputCouponGetConsumed = await couponGetById.execute(outputCouponCreate.couponId)
+    const outputUserGetAccount = await userRepository.getById(outputCouponConsume.userId)
+    expect(outputCouponGetConsumed.quantity).toBe(outputCouponGet.quantity - 1)
+    expect(outputUserGetAccount?.name).toBe(inputUserSignup.name)
 })
 
 test("Should not consume a coupon with a invalid coupon id", async function(){
-    await expect(() => couponConsume.execute(crypto.randomUUID())).rejects.toThrow(new Error("This coupon not exists"))
+    const inputUserSignup = {
+        name: "user user",
+        cpf: "91015490069",
+        email: `user${Math.random()}@user`,
+        phone: "(99) 9999-9999"
+    }
+    const outputUserSignup = await userSignup.execute(inputUserSignup)
+    await expect(() => couponConsume.execute({couponId: crypto.randomUUID(), userId: outputUserSignup.userId})).rejects.toThrow(new Error("This coupon not exists"))
 })
 
 test("Should not consume a coupon with a invalid coupon quantity", async function(){
+    const inputUserSignup = {
+        name: "user user",
+        cpf: "91015490069",
+        email: `user${Math.random()}@user`,
+        phone: "(99) 9999-9999"
+    }
+    const outputUserSignup = await userSignup.execute(inputUserSignup)
     const inputCompany = {
         isCompany: true,
         name: "company company",
@@ -76,7 +104,7 @@ test("Should not consume a coupon with a invalid coupon quantity", async functio
         quantity: 0
     }
     const outputCreateCoupon = await couponCreate.execute(inputCoupon)
-    await expect(() => couponConsume.execute(outputCreateCoupon.couponId)).rejects.toThrow(new Error("This coupon doesn't have enough quantity to be consumed"))
+    await expect(() => couponConsume.execute({couponId: outputCreateCoupon.couponId, userId: outputUserSignup.userId})).rejects.toThrow(new Error("This coupon doesn't have enough quantity to be consumed"))
 })
 
 afterEach(async () => {
