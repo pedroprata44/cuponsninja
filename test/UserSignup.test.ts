f344850f75a7fd93f4e711113f72b3b70c09934c
pedroprata44 <contato.pedroprata@gmail.com>
2024-06-20 14:29:41 -0300

    test: update usersignup test to use value objects

--- test/UserSignup.test.ts
+++ test/UserSignup.test.ts
@@ -5,6 +5,10 @@ import UserRepository from "../src/application/repository/UserRepository"
 import PgPromiseAdapter from "../src/infra/database/PgPromiseAdapter"
 import DatabaseConnection from "../src/infra/database/DatabaseConnection"
 import LoggerConsole from "../src/infra/logger/LoggerConsole"
+import Name from "../src/domain/Name"
+import Cpf from "../src/domain/Cpf"
+import Email from "../src/domain/Email"
+import Phone from "../src/domain/Phone"
 
 
 let userSignup: UserSignup
@@ -19,18 +23,18 @@ beforeEach(() => {
     userGetAccount = new UserGetAccount(userRepositoryDatabase)
 })
 
-test("Should do user signup", async function(){
-    const inputSignup = {
+test.only("Should do user signup", async function(){
+    const inputUserSignup = {
         name: "user user",
         cpf: "91015490069",
         email: `user${Math.random()}@user`,
         phone: "(99) 9999-9999"
     }
-    const outputSignup = await userSignup.execute(inputSignup)
+    const outputSignup = await userSignup.execute(inputUserSignup)
     const outputGetAccount = await userGetAccount.execute(outputSignup.userId)
-    expect(outputGetAccount.id).toBeDefined()
-    expect(outputGetAccount.name).toBe(inputSignup.name)
-    expect(outputGetAccount.email).toBe(inputSignup.email)
+    expect(outputGetAccount.userId).toBeDefined()
+    expect(outputGetAccount.name).toBe(inputUserSignup.name)
+    expect(outputGetAccount.email).toBe(inputUserSignup.email)
 })
 
 test("Should not do signup user with a email already exists", async function(){
@@ -46,15 +50,15 @@ test("Should not do signup user with a email already exists", async function(){
             return users.find((user: any) => user.email === userEmail)
         }
     }
-    const inputSignup = {
+    const inputUserSignup = {
         name: "user user",
         cpf: "91015490069",
         email: `user${Math.random()}@user`,
         phone: "(99) 9999-9999"
     }
     const userSignup = new UserSignup(userRepository, new LoggerConsole())
-    await userSignup.execute(inputSignup)
-    expect(() => userSignup.execute(inputSignup)).rejects.toThrow(new Error("This email already exists"))
+    await userSignup.execute(inputUserSignup)
+    expect(() => userSignup.execute(inputUserSignup)).rejects.toThrow(new Error("This email already exists"))
 })
 
 afterEach(async () => {
@@ -63,45 +67,45 @@ afterEach(async () => {
 
 // test.each([undefined, null, "", "user"])("Should not do signup user with a invalid name", function(){
 //     const stubUserDAOGetByEmail = sinon.stub(UserRepositoryDatabase.prototype, "getByEmail").resolves(undefined)
-//     const inputSignup = {
+//     const inputUserSignup = {
 //         name: "user",
 //         cpf: "91015490069",
 //         email: `user${Math.random()}@user`,
 //         phone: "(99) 9999-9999"
 //     }
-//     expect(() => userSignup.execute(inputSignup)).rejects.toThrow(new Error("Invalid name"))
+//     expect(() => userSignup.execute(inputUserSignup)).rejects.toThrow(new Error("Invalid name"))
 //     stubUserDAOGetByEmail.restore()
 // })
 
 // test.each([undefined,null,"","user.user"])("Should do not signup with a invalid email", function(email:any){
 //     const stubUserDAOGetByEmail = sinon.stub(UserRepositoryDatabase.prototype, "getByEmail").resolves(undefined)
-//     const inputSignup = {
+//     const inputUserSignup = {
 //         email: email,
 //         name: "user user",
 //     }
-//     expect(() => userSignup.execute(inputSignup)).rejects.toThrow("Invalid email")
+//     expect(() => userSignup.execute(inputUserSignup)).rejects.toThrow("Invalid email")
 //     stubUserDAOGetByEmail.restore()
 // })
 
 // test.each([undefined, null, "", "111", "11111111111", "46890347810"])("Should do not signup with a invalid cpf", function(cpf:any){
 //     const stubUserDAOGetByEmail = sinon.stub(UserRepositoryDatabase.prototype, "getByEmail").resolves(undefined)
-//     const inputSignup = {
+//     const inputUserSignup = {
 //         email: `user${Math.random()}@user`,
 //         name: "user user",
 //         cpf: cpf
 //     }
-//     expect(() => userSignup.execute(inputSignup)).rejects.toThrow("Invalid cpf")
+//     expect(() => userSignup.execute(inputUserSignup)).rejects.toThrow("Invalid cpf")
 //     stubUserDAOGetByEmail.restore()
 // })
 
 // test.each([undefined, null, "", "() 0000-0000", "(00) 00000000", "0000000000"])("Should not do signup user with a invalid phone", function(phone:any){
 //     const stubUserDAOGetByEmail = sinon.stub(UserRepositoryDatabase.prototype, "getByEmail").resolves(undefined)
-//     const inputSignup = {
+//     const inputUserSignup = {
 //         email: `user${Math.random()}@user`,
 //         name: "user user",
 //         cpf: "91015490069",
 //         phone: phone
 //     }
-//     expect(() => userSignup.execute(inputSignup)).rejects.toThrow(new Error("Invalid phone"))
+//     expect(() => userSignup.execute(inputUserSignup)).rejects.toThrow(new Error("Invalid phone"))
 //     stubUserDAOGetByEmail.restore()
 // })
